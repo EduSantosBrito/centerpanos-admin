80482f78c7693231ed6af70907f28a3daa11dadb
EduSantosBrito <edu.santos.brito@gmail.com>
2021-10-19 20:16:26 -0300

    feature: include Banner

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,13 @@ import logger from 'redux-logger';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { composeWithDevTools } from 'redux-devtools-extension';
 import AuthReducer, { State as AuthState } from '~/ducks/auth';
+import BannerReducer, { State as BannerState } from '~/ducks/banner';
 
-export type RootState = { auth: AuthState };
+export type RootState = { auth: AuthState; banner: BannerState };
 
 const reducer = combineReducers({
     auth: AuthReducer,
+    banner: BannerReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger)));
